f2e89074b5bb8de4dbee030d5ddc02232bfa9a90
Robin LANGEOIRE <rlangeoi@e3r4p6.42.fr>
2018-01-14 22:22:26 +0100

    historique des commandes

---
+++ user_orders.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+$css_path = "./";
+$css_file = "admin.css";
+include('partial/head.php');
+include('partial/header.php');
+?>
+<body>
+	<div class="container">
+		<div>
+			<h4>Vos commandes</h4>
+			<div>
+				<div class="elem user-top first-row">
+					<p>Date</p>
+					<p>Articles</p>
+				</div>
+				<?php
+				include('tools/get_db.php');
+				$db = get_db('db', 'db/orders');
+				foreach ($db as $elem) {
+					if ($elem[2] == $_SESSION['logged_on_user'])
+					{
+					echo "<div class='elem-row user-row'>";
+					echo "<div class='elem user'>";
+					echo "<p>".$elem[1]."</p>";
+					echo "<p>".$elem[2]."</p>";
+					foreach($elem[3] as $name => $qte)
+						echo "<p>".$name.":".$qte."</p>";
+					echo "</p>";
+					echo "</div>";
+					echo "</div>";
+					}
+				}
+				?>
+			</div>
+
+		</div>
+	</div>
+
+	<?php include('partial/footer.php'); ?>
+</body>
+</html>
