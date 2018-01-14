5825684039451329e356ad3a1b4349e2a7df2c9d
Robin LANGEOIRE <rlangeoi@e3r4p6.42.fr>
2018-01-14 21:19:17 +0100

    validate_basket

---
+++ admin_basket.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+include('partial/admin_only.php');
+
+$css_path = "./";
+$css_file = "admin.css";
+include('partial/head.php');
+include('partial/header.php');
+?>
+<body>
+	<div class="container">
+		<div>
+			<h4>Liste des commandes</h4>
+			<div>
+				<div class="elem user-top first-row">
+					<p>Date</p>
+					<p>User</p>
+					<p>Articles</p>
+				</div>
+				<?php
+				include('tools/get_db.php');
+				$db = get_db('db', 'db/orders');
+				foreach ($db as $elem) {
+					echo "<div class='elem-row user-row'>";
+					echo "<div class='elem user'>";
+					echo "<p>".$elem[1]."</p>";
+					echo "<p>".$elem[2]."</p>";
+					foreach($elem[3] as $name => $qte)
+						echo "<p>".$name.":".$qte."</p>";
+					echo "</p>";
+					echo "</div>";
+					echo "</div>";
+				}
+				?>
+			</div>
+
+		</div>
+	</div>
+
+	<?php include('partial/footer.php'); ?>
+</body>
+</html>
